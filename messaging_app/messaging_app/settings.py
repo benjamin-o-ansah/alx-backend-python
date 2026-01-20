077683aa51169d646c67d20383a5704724be2080
benoansah2025 <memphisperla@gmail.com>
2026-01-20 11:31:04 +0000

    modified settings.py file concerning pagination

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -71,7 +71,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    'PageNumberPagination': 20,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    )
 }
 
 TEMPLATES = [
